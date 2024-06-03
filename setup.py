45c76794c765e0a095b4a06c6fb0bc9e812d6f4e
Tanguy PEMEJA <74564644+tpemeja@users.noreply.github.com>
2024-06-03 11:38:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fastmock",
-    version="0.1.0",
+    version="0.1.1",
     author="Tanguy PEMEJA",
     author_email="tanguy.pemeja@gmail.com",
     description="FastAPI middleware for mocking responses based on Pydantic models response",
